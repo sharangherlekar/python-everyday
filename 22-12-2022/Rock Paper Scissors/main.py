8327f5cc45155918baf7ba747f9086d26d09f22d
Sharang <sharangherlekar@gmail.com>
2022-12-22 17:31:49 +0530

    commit

---
+++ 22-12-2022/Rock Paper Scissors/main.py
@@ -0,0 +1,29 @@
+import random
+
+choices = ['rock', 'paper', 'scissors']
+computer = random.choice(choices)
+player = None
+while player not in choices:
+    player = input("rock paper or scissors?").lower()
+
+print("computer:", computer)
+print("player: " , player)
+
+if computer == player:
+    print("tie!")
+elif player =='rock':
+    if computer == 'paper':
+        print('you lose')
+    if computer == 'scissors':
+        print('you win!')
+elif player =='scissors':
+    if computer == 'rock':
+        print('you lose')
+    if computer == 'paper':
+        print('you win!')
+elif player =='paper':
+    if computer == 'scissors':
+        print('you lose')
+    if computer == 'rock':
+        print('you win!')
+
